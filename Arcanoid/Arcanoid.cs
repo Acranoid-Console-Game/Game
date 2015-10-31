89316d5f855e3e5782325a431bc8ae248dda544f
Martin <sirjordan1988@gmail.com>
2015-10-31 11:40:16 +0200

    Project uploaded

---
+++ Arcanoid/Arcanoid.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        int ballTop = 10;
+        int ballLeft = 8;
+
+        int topDirection = -1;
+        int leftDirection = -1;
+
+        while (true)
+        {
+            if (ballLeft == 0)
+            {
+                leftDirection = 1;
+            }
+            if (ballLeft == 79)
+            {
+                leftDirection = -1;
+            }
+
+            if (ballTop == 0)
+            {
+                topDirection = 1;
+            }
+            if (ballTop == 22)
+            {
+                topDirection = -1;
+            }
+
+            ballLeft = ballLeft + leftDirection;
+            ballTop = ballTop + topDirection;
+           
+            Console.SetCursorPosition(ballLeft, ballTop);
+            Console.Write("@");
+
+            Console.SetCursorPosition(20, 22);
+            Console.WriteLine("========");
+
+            Thread.Sleep(100);
+            Console.Clear();
+        }
+    }
+}
