ae9e467e00f1b7a8f9db4cacf18d4b9458bda8e1
Martin <sirjordan1988@gmail.com>
2015-11-04 21:48:16 +0200

    Game updated

--- Arcanoid/Arcanoid.cs
+++ Arcanoid/Arcanoid.cs
@@ -5,43 +5,88 @@ class Program
 {
     static void Main()
     {
-        int ballTop = 12;
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Blue;
+
+        int ballTop = 10;
         int ballLeft = 8;
+        int paddleLeft = 30;
+
+        int minLeft = 0;
+        int maxLeft = 79;
+        int minTop = 0;
+        int maxTop = 22;
 
         int topDirection = -1;
         int leftDirection = -1;
 
+        string paddle = "========";
+
         while (true)
         {
-            if (ballLeft == 0)
+            if (ballLeft == minLeft)
             {
                 leftDirection = 1;
             }
-            if (ballLeft == 79)
+
+            if (ballLeft == maxLeft)
             {
                 leftDirection = -1;
             }
 
-            if (ballTop == 0)
+            if (ballTop == minTop)
             {
                 topDirection = 1;
             }
-            if (ballTop == 22)
-            {
-                topDirection = -1;
-            }
 
+            if (ballTop == maxTop)
+            {
+                if (ballLeft >= paddleLeft && ballLeft <= paddleLeft + paddle.Length)
+                {
+                    topDirection = -1;
+                }
+                else
+                {
+                    // Game over
+                    break;
+                }
+            }
+            
             ballLeft = ballLeft + leftDirection;
             ballTop = ballTop + topDirection;
-           
+
             Console.SetCursorPosition(ballLeft, ballTop);
             Console.Write("@");
 
-            Console.SetCursorPosition(20, 22);
-            Console.WriteLine("========");
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo pressed = Console.ReadKey();
+                if (pressed.Key == ConsoleKey.LeftArrow)
+                {
+                    if (paddleLeft > minLeft)
+                    {
+                        // Move left
+                        paddleLeft = paddleLeft - 2;
+                    }
+                }
+                if (pressed.Key == ConsoleKey.RightArrow)
+                {
+                    if (paddleLeft <= maxLeft - paddle.Length)
+                    {
+                        // Move right
+                        paddleLeft = paddleLeft + 2;
+                    }
+                }
+            }
+
+            Console.SetCursorPosition(paddleLeft, maxTop);
+            Console.WriteLine(paddle);
 
             Thread.Sleep(100);
             Console.Clear();
         }
+        
+        Console.WriteLine("---GAME OVER---");
+        Console.ReadLine();
     }
 }
